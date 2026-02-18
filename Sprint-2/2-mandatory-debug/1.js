e8780d598848b6e78d0934b2ccaf0e83d6308c03
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-02-18 12:09:11 +0000

    debug 1

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
-
+//  =============> write your prediction here - should show undefined as there are ";" after return inside the function
 function sum(a, b) {
   return;
   a + b;
@@ -10,4 +9,9 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> write your new code here - you can't divide the return parameters with ";"
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
