c9ce239c3631545fbf47fa3fa2ffb68698282669
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-02-18 12:01:18 +0000

    error 2

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,21 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> write your prediction of the error here - num is not defined
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
 
-// =============> write the error message here
+// =============> write the error message here - SyntaxError: Unexpected number
 
-// =============> explain this error message here
+// =============> explain this error message here - you can't set parameter as a number
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
 
-
+console.log(square(3))
